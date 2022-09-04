94ee4985419982d793da4c4a94b5e8b9cadc79c8
Bruna Lima <missbruni@users.noreply.github.com>
2022-09-04 12:30:18 +0100

    fix overflow

--- app/root.tsx
+++ app/root.tsx
@@ -43,7 +43,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body className="h-full">
+      <body className="h-full w-full overflow-hidden">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
