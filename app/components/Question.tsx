fb9bf44732d66288c0de0736d6eab44b2910f9f1
bruna <bruna.lima@guestline.com>
2022-11-22 11:08:40 +0000

    adjust top padding question

--- app/components/Question.tsx
+++ app/components/Question.tsx
@@ -7,7 +7,7 @@ type QuestionProps = {
 
 const Question: React.FC<QuestionProps> = ({ question }) => {
   return (
-    <div className="lg:pb-18 flex h-full w-full flex-col items-center justify-center gap-20 px-4 pt-16 pb-8 sm:px-6 sm:pb-14 lg:px-8 lg:pt-32">
+    <div className="lg:pb-18 flex h-full w-full flex-col items-center justify-center gap-20 px-4 pt-16 pb-8 sm:px-6 sm:pb-14 lg:px-8">
       <h6 className="text-center text-2xl font-extrabold tracking-tight">
         <span className="block text-[#1dbab4] drop-shadow-md">Question</span>
       </h6>
